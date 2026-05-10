bb7b0e4bd6b2421f4d5cfd5c2aac90fae3a35260
Jaisheel15 <jaisheel021@gmail.com>
2026-05-10 22:18:35 +0530

    first committ

--- main.py
+++ main.py
@@ -59,13 +59,3 @@ def query_documents(request: QueryRequest):
     return generate_answer(_vector_store, request.question)
 
 
-def main():
-    import uvicorn
-    import os
-
-    port = int(os.environ.get("PORT", 8000))
-
-    uvicorn.run("main:app", host="0.0.0.0", port=port)
-
-if __name__ == "__main__":
-    main()
